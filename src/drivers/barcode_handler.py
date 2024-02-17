ea2d773cdf7e120eaa6d76d55bd9268fada3fcd1
João Jamba <joao.jamba@ideiasdinamicas.com>
2024-02-17 15:05:05 +0100

    separando as responsabilidades

---
+++ src/drivers/barcode_handler.py
@@ -0,0 +1,11 @@
+from barcode import Code128
+from barcode.writer import ImageWriter
+
+
+class BarcodeHandler:
+    def create_barcode(self, produc_code: str) -> str:
+        tag = Code128(produc_code, writer=ImageWriter())
+        path_from_tag = f"{tag}"
+        tag.save(path_from_tag)
+
+        return path_from_tag
